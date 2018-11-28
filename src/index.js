d5fe95f43e79ac53069a2fc06272c94609270a2f
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2018-11-28 18:51:13 +0800

    实现地图的展示

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import "./styles/index.scss"
-
-class A{
-    constructor(){
-        this.x = 123
-    }
-}
+import "./styles/common.scss"
+import * as d3 from "d3"
+import styleJson from "./utils/custom_map_config.json"
+let map = new BMap.Map("container")
+let point = new BMap.Point(116.404, 39.915) 
+map.centerAndZoom(point, 15)
+map.enableScrollWheelZoom()
+map.setMapStyleV2({styleJson: styleJson}) 
