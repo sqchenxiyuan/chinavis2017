48eb4baf937c7a0dfe9afa0a8b52a12d9dfd9fb6
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2018-12-26 20:35:00 +0800

    优化

--- src/app.vue
+++ src/app.vue
@@ -17,9 +17,9 @@
                 <button @click="clearSelect">清除选择</button>
                 <button :class="{'active':activeType==='BARS'}" class="group1" @click="updateMapType('BARS')" style="padding:0 11px;">网吧</button>
                 <button :class="{'active':activeType==='INTERNET_RECORD'}" @click="updateMapType('INTERNET_RECORD')">上网记录</button>
+                <button :class="{'active':activeType==='UNDER_AGE'}" @click="updateMapType('UNDER_AGE')">未成年</button>
                 <button :class="{'active':activeType==='FLOAT_PERSON'}" @click="updateMapType('FLOAT_PERSON')">流动人口</button>
-                <button :class="{'active':activeType==='UNDER_AGE'}" @click="updateMapType('UNDER_AGE')">未成年人口</button>
-                <button class="group2" @click="downloadData">下载筛选数据</button>
+                <button class="group2" @click="downloadData">下载数据</button>
             </div>
             <div style="flex: 4">
                 <button :class="{'active':timeInterval === 24 * 3600}" @click="selelctShowTimeTnterval(24 * 3600)">1D</button>
